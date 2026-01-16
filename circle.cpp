c36cb2e8b5a6a31312c485043350a8dbe59c0095
medc-0 <moonisontop76@gmail.com>
2026-01-16 18:17:17 +0100

    chore: implement simple window drawing and renderer

---
+++ circle.cpp
@@ -0,0 +1,39 @@
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+#include <iostream>
+
+#define WIDTH 900
+#define HEIGHT 600
+
+int main() {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("Hello SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        while(SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) running = false;
+        }
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
+    return 0;
+}
--- sphere.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "hello, ball\n";
-    return 0;
-}
